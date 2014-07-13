586917a57da6159685c8f86e6806d5cd13604e83
Kim Blanquera <kim.blanquera@gmail.com>
2014-07-13 11:58:04 +0800

    input stuff

--- src/UI/CategoryManagement.java
+++ src/UI/CategoryManagement.java
@@ -119,7 +119,7 @@ public class CategoryManagement extends javax.swing.JFrame {
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel3))
                         .addGap(62, 62, 62)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -165,7 +165,7 @@ public class CategoryManagement extends javax.swing.JFrame {
                             .addComponent(jButton3))))
                 .addGap(28, 28, 28)
                 .addComponent(jButton5)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(159, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
