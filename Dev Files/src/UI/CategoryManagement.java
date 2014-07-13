d8683a2c1189b9e4ca80ec800590a682a11e9fef
Clark <claaarkabalooos@gmail.com>
2014-07-13 19:10:11 +0800

    [CLARK]: Fixed CategoryManagement.

--- Dev Files/src/UI/CategoryManagement.java
+++ Dev Files/src/UI/CategoryManagement.java
@@ -288,7 +288,7 @@ public class CategoryManagement extends javax.swing.JFrame {
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2)
                             .addComponent(jLabel3))
                         .addGap(62, 62, 62)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -334,7 +334,6 @@ public class CategoryManagement extends javax.swing.JFrame {
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(18, 18, 18)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-<<<<<<< HEAD
                             .addComponent(jLabel3)
                             .addComponent(categoryNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(errorLabel1))
@@ -343,13 +342,6 @@ public class CategoryManagement extends javax.swing.JFrame {
                             .addComponent(btnUpdateCategory)
                             .addComponent(btnClear))
                         .addGap(22, 22, 22))))
-=======
-                            .addComponent(jButton4)
-                            .addComponent(jButton3))))
-                .addGap(28, 28, 28)
-                .addComponent(jButton5)
-                .addContainerGap(159, Short.MAX_VALUE))
->>>>>>> actualInput
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
