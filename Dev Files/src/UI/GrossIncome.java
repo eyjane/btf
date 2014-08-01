1021e7ab428df827e666742a8a5ef2104a89bacb
Kim Blanquera <kim.blanquera@gmail.com>
2014-08-01 14:30:36 +0800

    Chart generation

--- Dev Files/src/UI/GrossIncome.java
+++ Dev Files/src/UI/GrossIncome.java
@@ -13,6 +13,7 @@ import DAO.Implementation.SalesDAOImplementation;
 import DAO.Interface.IngredientDAOInterface;
 import DAO.Interface.RecipeDAOInterface;
 import DAO.Interface.SalesDAOInterface;
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -23,9 +24,14 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Date;
+import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.table.DefaultTableModel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.chart.ChartPanel;
 /**
  *
  * @author Evy
@@ -41,6 +47,9 @@ public class GrossIncome extends javax.swing.JFrame {
     public GrossIncome() {
         initComponents();
         GITable();
+        expTable();
+        netTable();
+        makeGIChart();
     }
 
     /**
@@ -63,8 +72,14 @@ public class GrossIncome extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jPanelChart = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowActivated(java.awt.event.WindowEvent evt) {
+                formWindowActivated(evt);
+            }
+        });
 
         jLabel3.setFont(new java.awt.Font("Lucida Grande", 1, 36)); // NOI18N
         jLabel3.setText("Gross Income");
@@ -140,6 +155,17 @@ public class GrossIncome extends javax.swing.JFrame {
 
         jLabel4.setText("Net Income");
 
+        javax.swing.GroupLayout jPanelChartLayout = new javax.swing.GroupLayout(jPanelChart);
+        jPanelChart.setLayout(jPanelChartLayout);
+        jPanelChartLayout.setHorizontalGroup(
+            jPanelChartLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanelChartLayout.setVerticalGroup(
+            jPanelChartLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 353, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -148,8 +174,17 @@ public class GrossIncome extends javax.swing.JFrame {
                 .addComponent(jButton1)
                 .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
+                .addGap(114, 114, 114)
+                .addComponent(jLabel1)
+                .addGap(251, 251, 251)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel4)
+                .addGap(180, 180, 180))
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanelChart, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel3)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -158,14 +193,6 @@ public class GrossIncome extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(73, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(114, 114, 114)
-                .addComponent(jLabel1)
-                .addGap(251, 251, 251)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel4)
-                .addGap(180, 180, 180))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -184,13 +211,22 @@ public class GrossIncome extends javax.swing.JFrame {
                         .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(382, 382, 382)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanelChart, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(jButton1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
+        // TODO add your handling code here:
+        
+        
+        
+    }//GEN-LAST:event_formWindowActivated
 
     /*
      *  <!-- KIM CODE STARTS HERE -->
@@ -284,6 +320,76 @@ public class GrossIncome extends javax.swing.JFrame {
             tableColumn.setPreferredWidth(preferredWidth);
         }
     }
+    
+    public void makeGIChart() {
+        
+        DefaultPieDataset objDataset = new DefaultPieDataset();
+        int rows = grossIncomeTable.getRowCount();
+        
+        for (int c = 0; c < rows; c++) {
+            
+            objDataset.setValue(grossIncomeTable.getValueAt(c, 0).toString(), Float.parseFloat(grossIncomeTable.getValueAt(c, 1).toString())); // retrieve table contents and add to data set
+            
+        }
+        
+        JFreeChart grossChart = ChartFactory.createPieChart("Gross Income", objDataset, true, true, false); // makes chart
+        ChartPanel chartPanel = new ChartPanel(grossChart);
+        jPanelChart.setLayout(new BorderLayout());
+        jPanelChart.add(chartPanel, BorderLayout.NORTH);
+       
+        JFrame frame = new JFrame();
+        frame.add(jPanelChart);
+        frame.pack();
+        frame.setVisible(true);
+        
+    }
+    
+    public void makeExpChart() {
+        
+        DefaultPieDataset objDataset = new DefaultPieDataset();
+        int rows = expensesTable.getRowCount();
+        
+        for (int c = 0; c < rows; c++) {
+            
+            objDataset.setValue(expensesTable.getValueAt(c, 0).toString(), Float.parseFloat(expensesTable.getValueAt(c, 1).toString())); // retrieve table contents and add to data set
+            
+        }
+        
+        JFreeChart expChart = ChartFactory.createPieChart("Gross Income", objDataset, true, true, false); // makes chart
+        ChartPanel chartPanel = new ChartPanel(expChart);
+        jPanelChart.setLayout(new BorderLayout());
+        jPanelChart.add(chartPanel, BorderLayout.NORTH);
+       
+        JFrame frame = new JFrame();
+        frame.add(jPanelChart);
+        frame.pack();
+        frame.setVisible(true);
+        
+    }
+    
+    public void makeNetChart() {
+        
+        DefaultPieDataset objDataset = new DefaultPieDataset();
+        int rows = netIncomeTable.getRowCount();
+        
+        for (int c = 0; c < rows; c++) {
+            
+            objDataset.setValue(netIncomeTable.getValueAt(c, 0).toString(), Float.parseFloat(netIncomeTable.getValueAt(c, 1).toString())); // retrieve table contents and add to data set
+            
+        }
+        
+        JFreeChart netChart = ChartFactory.createPieChart("Gross Income", objDataset, true, true, false); // makes chart
+        ChartPanel chartPanel = new ChartPanel(netChart);
+        jPanelChart.setLayout(new BorderLayout());
+        jPanelChart.add(chartPanel, BorderLayout.NORTH);
+       
+        JFrame frame = new JFrame();
+        frame.add(jPanelChart);
+        frame.pack();
+        frame.setVisible(true);
+        
+    }
+    
     /*
      *  <!-- KIM CODE ENDS HERE --> 
      */
@@ -331,6 +437,7 @@ public class GrossIncome extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanelChart;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
